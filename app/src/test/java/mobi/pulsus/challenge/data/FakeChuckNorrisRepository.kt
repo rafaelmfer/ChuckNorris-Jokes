832366f126ceaca07743dad6a0515f6c22d8abd1
Rafael Ferreira <rafaelmfer@hotmail.com>
2022-10-09 15:12:10 -0300

    :test_tube: test: criado Random Joke ViewModel Test

---
+++ app/src/test/java/mobi/pulsus/challenge/data/FakeChuckNorrisRepository.kt
@@ -0,0 +1,74 @@
+package mobi.pulsus.challenge.data
+
+import mobi.pulsus.challenge.domain.model.JokeModel
+import mobi.pulsus.challenge.domain.model.SearchJokeModel
+import mobi.pulsus.challenge.domain.repository.IChuckNorrisRepository
+
+// Use FakeDataSource that acts as a test double to the LocalDataSource
+class FakeChuckNorrisRepository : IChuckNorrisRepository {
+
+    companion object {
+        val jokeTest = JokeModel(
+            categories = listOf(),
+            createdAt = "20/10/2222",
+            iconUrl = "test",
+            id = "test_id",
+            url = "test.com",
+            value = "This is a test joke"
+        )
+        val categoriesTest = listOf("animal", "dev", "food")
+    }
+
+    private var shouldReturnError = false
+
+    fun shouldReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
+    override suspend fun getRandomJoke(): JokeModel {
+        if (shouldReturnError) {
+            throw Exception()
+        }
+
+        return jokeTest
+    }
+
+    override suspend fun getCategories(): List<String> {
+        if (shouldReturnError) {
+            throw Exception()
+        }
+
+        return categoriesTest
+    }
+
+    override suspend fun getRandomJokeFromCategory(categoryName: String): JokeModel {
+        if (shouldReturnError) {
+            throw Exception()
+        }
+
+        if (categoryName == "") {
+            throw Exception()
+        } else {
+            return jokeTest
+        }
+    }
+
+    override suspend fun getJokesFromAKeyword(keyword: String): SearchJokeModel {
+        if (shouldReturnError) {
+            throw Exception()
+        }
+
+        if (keyword.length >= 3) {
+            return SearchJokeModel(
+                total = 3,
+                result = listOf(
+                    jokeTest,
+                    jokeTest,
+                    jokeTest
+                )
+            )
+        } else {
+            throw Exception()
+        }
+    }
+}
