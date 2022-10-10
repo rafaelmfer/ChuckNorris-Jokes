f9176fe02b977d872594b4b94fa14ea5b353411c
Rafael Ferreira <rafaelmfer@hotmail.com>
2022-10-09 22:15:44 -0300

    :recycle: refactor: mudança do botão voltar e fechar da custom view

--- commons/src/main/java/mobi/pulsus/challenge/commons/customviews/toolbars/GenericToolbar.kt
+++ commons/src/main/java/mobi/pulsus/challenge/commons/customviews/toolbars/GenericToolbar.kt
@@ -5,13 +5,12 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.View
-import android.view.accessibility.AccessibilityEvent
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import com.google.android.material.button.MaterialButton
 import mobi.pulsus.challenge.commons.R
 import mobi.pulsus.challenge.commons.extensions.configureBackButtonAction
-import mobi.pulsus.challenge.commons.extensions.onSingleClick
 import mobi.pulsus.challenge.commons.extensions.setAccessibilityAsButton
 import mobi.pulsus.challenge.commons.extensions.setAccessibilityAsHeader
 import mobi.pulsus.challenge.commons.extensions.setToolbarAccessibleBackButton
@@ -19,17 +18,15 @@ import mobi.pulsus.challenge.commons.extensions.setToolbarAccessibleBackButton
 class GenericToolbar @JvmOverloads
 constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private val btLeftButton by lazy { findViewById<TextView>(R.id.tv_generic_toolbar_left_button) }
+    private val btLeftButton by lazy { findViewById<MaterialButton>(R.id.mbt_generic_toolbar_left_button) }
     private val tvToolbarTitle by lazy { findViewById<TextView>(R.id.tv_generic_toolbar_title) }
-    private val btRightButton by lazy { findViewById<TextView>(R.id.tv_generic_toolbar_right_button) }
+    private val btRightButton by lazy { findViewById<MaterialButton>(R.id.mbt_generic_toolbar_right_button) }
 
     private var showLeftButton: Boolean
     private var showTitle: Boolean = true
     private var showRightButton: Boolean
 
     private var attributesTypedArray: TypedArray
-
-    var titleToolbar: String? = null
 
     init {
         inflate(context, R.layout.view_generic_toolbar, this)
@@ -69,43 +66,5 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
         btLeftButton.visibility = if (showLeftButton) View.VISIBLE else View.GONE
         tvToolbarTitle.visibility = if (showTitle) View.VISIBLE else View.GONE
         btRightButton.visibility = if (showRightButton) View.VISIBLE else View.GONE
-    }
-
-    fun setLeftButtonAction(action: () -> Unit) {
-        btLeftButton.onSingleClick { action() }
-    }
-
-    fun setRightButtonAction(action: () -> Unit) {
-        btRightButton.onSingleClick { action() }
-    }
-
-    fun updateTitleToolbar() {
-        tvToolbarTitle.run {
-            text = titleToolbar
-            contentDescription = resources.getString(R.string.generic_toolbar_title_acs, titleToolbar)
-        }
-    }
-
-    fun setFocusOnLeftButton() {
-        btLeftButton.requestFocus()
-        btLeftButton.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_ACCESSIBILITY_FOCUSED)
-    }
-
-    fun setLeftButtonText(text: String) {
-        btLeftButton.text = text
-        btLeftButton.contentDescription = text
-    }
-
-    fun setLeftButtonColor(color: Int) {
-        btLeftButton.setTextColor(color)
-    }
-
-    fun setRightButtonText(text: String) {
-        btRightButton.text = text
-        btRightButton.contentDescription = text
-    }
-
-    fun setRightButtonColor(color: Int) {
-        btRightButton.setTextColor(color)
     }
 }
